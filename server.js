f906ba4dfdf966fed774152d22ae5ebafabc8704
Josh Ho <josh.ho@slalom.com>
2020-11-03 16:57:46 -0800

    Working simple implementation

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { graphqlHTTP } = require('express-graphql');  //Need to use destructuring assignment here to assign the function
-const schema = require('./schema.js')
+const schema = require('./schema.js');
+const cors = require('cors');
 
 const app = express();
+
+app.use( cors() ); //We know why we need this
 
 //Basic integration of our schema with Graph QL - Route all requests on /graphql to GraphQL
 app.use('/graphql', graphqlHTTP({
